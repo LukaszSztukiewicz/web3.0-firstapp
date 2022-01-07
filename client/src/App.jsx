4252c99d3553d3f89b0f0e817b20f837fb9f9f86
Łukasz Sztukiewicz <sztukiewicz.lukasz@gmail.com>
2022-01-07 14:52:41 +0100

    Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,15 @@ import { Navbar, Welcome, Footer, Services, Transactions } from "./components"
 const  App = () => {
   return (
     <div className="App">
-     <h1 className="text-3xl font-bold underline">
-       Hello world tailwindCss!
-     </h1>
+     <div className="min-h-screen">
+       <div className="gradient-bg-welcome">
+         <Navbar />
+         <Welcome />
+       </div>
+       <Services />
+       <Transactions/>
+       <Footer />
+     </div>
     </div>
   )
 }
