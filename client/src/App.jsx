4513c45da8f4147bde136b131a34040a6283c4f4
Łukasz Sztukiewicz <sztukiewicz.lukasz@gmail.com>
2022-01-07 11:54:50 +0100

    initial setup

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { Navbar, Welcome, Footer, Services, Transactions } from "./components"
+const  App = () => {
+  return (
+    <div className="App">
+     <h1 className="text-3xl font-bold underline">
+       Hello world tailwindCss!
+     </h1>
+    </div>
+  )
+}
+
+export default App
